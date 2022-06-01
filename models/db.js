5563be1ed98a3da0d8eb7d2f02ac14b85a48acaf
sang2xx2 <s191102g@gmail.com>
2022-06-01 15:31:46 +0700

    fix

--- models/db.js
+++ models/db.js
@@ -1,17 +1,17 @@
 var mysql = require('mysql');
-// var db = mysql.createConnection({
-//     host: 'us-cdbr-east-05.cleardb.net',
-//     user: 'b4974887c1c471',
-//     password: '6b304aef',
-//     database: 'heroku_db08387a40c3c0f'
-// } );
-
 var db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'shop'
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'b4974887c1c471',
+    password: '6b304aef',
+    database: 'heroku_db08387a40c3c0f'
 } );
+
+// var db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'shop'
+// } );
 db.connect(() => console.log('Da ket noi database !'));
 
 db.on('error', function(err) {
